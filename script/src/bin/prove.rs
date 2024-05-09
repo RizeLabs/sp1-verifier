fd9d31a3c1cd2342ab3a3f1cc2a3909f70f4eeb5
nlok5923 <nlokhande5923@gmail.com>
2024-05-09 22:38:39 +0530

    added zkevm verification

--- script/src/bin/prove.rs
+++ script/src/bin/prove.rs
@@ -12,6 +12,7 @@ use alloy_sol_types::{sol, SolType};
 use clap::Parser;
 use serde::{Deserialize, Serialize};
 use sp1_sdk::{HashableKey, ProverClient, SP1Stdin};
+use std::time::Instant;
 
 /// The ELF (executable and linkable format) file for the Succinct RISC-V zkVM.
 ///
@@ -56,51 +57,58 @@ fn main() {
     // Setup the program.
     let (pk, vk) = client.setup(FIBONACCI_ELF);
 
-    // Setup the inputs.;
     let mut stdin = SP1Stdin::new();
     stdin.write(&args.n);
 
+    let start_proving_time = Instant::now();
     // Generate the proof.
     let proof = client
         .prove_groth16(&pk, stdin)
         .expect("failed to generate proof");
+    let end_time = Instant::now();
 
-    // Deserialize the public values.
-    let bytes = proof.public_values.as_slice();
-    let (n, a, b) = PublicValuesTuple::abi_decode(bytes, false).unwrap();
+    let end_proving_time = end_time.duration_since(start_proving_time);
+    println!(
+        "Elapsed time in g16 proof generation: {:?}",
+        end_proving_time
+    );
 
-    // Create the testing fixture so we can test things end-ot-end.
-    let fixture = SP1FibonacciProofFixture {
-        a,
-        b,
-        n,
-        vkey: vk.bytes32().to_string(),
-        public_values: proof.public_values.bytes().to_string(),
-        proof: proof.bytes().to_string(),
-    };
+    // // Deserialize the public values.
+    // let bytes = proof.public_values.as_slice();
+    // let (n, a, b) = PublicValuesTuple::abi_decode(bytes, false).unwrap();
 
-    // The verification key is used to verify that the proof corresponds to the execution of the
-    // program on the given input.
-    //
-    // Note that the verification key stays the same regardless of the input.
-    println!("Verification Key: {}", fixture.vkey);
+    // // Create the testing fixture so we can test things end-ot-end.
+    // let fixture = SP1FibonacciProofFixture {
+    //     a,
+    //     b,
+    //     n,
+    //     vkey: vk.bytes32().to_string(),
+    //     public_values: proof.public_values.bytes().to_string(),
+    //     proof: proof.bytes().to_string(),
+    // };
 
-    // The public values are the values whicha are publically commited to by the zkVM.
-    //
-    // If you need to expose the inputs or outputs of your program, you should commit them in
-    // the public values.
-    println!("Public Values: {}", fixture.public_values);
+    // // The verification key is used to verify that the proof corresponds to the execution of the
+    // // program on the given input.
+    // //
+    // // Note that the verification key stays the same regardless of the input.
+    // println!("Verification Key: {}", fixture.vkey);
 
-    // The proof proves to the verifier that the program was executed with some inputs that led to
-    // the give public values.
-    println!("Proof Bytes: {}", fixture.proof);
+    // // The public values are the values whicha are publically commited to by the zkVM.
+    // //
+    // // If you need to expose the inputs or outputs of your program, you should commit them in
+    // // the public values.
+    // println!("Public Values: {}", fixture.public_values);
 
-    // Save the fixture to a file.
-    let fixture_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../contracts/src/fixtures");
-    std::fs::create_dir_all(&fixture_path).expect("failed to create fixture path");
-    std::fs::write(
-        fixture_path.join("fixture.json"),
-        serde_json::to_string_pretty(&fixture).unwrap(),
-    )
-    .expect("failed to write fixture");
+    // // The proof proves to the verifier that the program was executed with some inputs that led to
+    // // the give public values.
+    // println!("Proof Bytes: {}", fixture.proof);
+
+    // // Save the fixture to a file.
+    // let fixture_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../contracts/src/fixtures");
+    // std::fs::create_dir_all(&fixture_path).expect("failed to create fixture path");
+    // std::fs::write(
+    //     fixture_path.join("fixture.json"),
+    //     serde_json::to_string_pretty(&fixture).unwrap(),
+    // )
+    // .expect("failed to write fixture");
 }
